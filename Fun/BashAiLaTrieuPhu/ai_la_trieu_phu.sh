8d1388c7eb7395bf81796e186527d0e0aa55fd71
nvat <24392632+nvatuan@users.noreply.github.com>
2020-10-06 05:36:50 +0000

    Add files via upload

--- Fun/BashAiLaTrieuPhu/ai_la_trieu_phu.sh
+++ Fun/BashAiLaTrieuPhu/ai_la_trieu_phu.sh
@@ -5,11 +5,15 @@ function get_question {
 
 	firstline=$( expr $1 '*' 6 - 5)
 	lastline=$( expr $firstline + 5)
-	for i in $(seq $firstline 1 $lastline)
-	do 
-		echo "line = $i"
-		echo $(head -n $i $file | tail -n 1);
-	done
+
+	question=$(head -n $(expr $firstline) $file | tail -n 1);
+	optionA=$(head -n $(expr $firstline + 1) $file | tail -n 1);
+	optionB=$(head -n $(expr $firstline + 2) $file | tail -n 1);
+	optionC=$(head -n $(expr $firstline + 3) $file | tail -n 1);
+	optionD=$(head -n $(expr $firstline + 4) $file | tail -n 1);
+	answer=$(head -n $(expr $firstline + 5) $file | tail -n 1);
+	
+	zenity --text="$question" --list --radiolist --column "" --column "Lua chon" FALSE $optionA FALSE $optionB FALSE $optionC FALSE $optionD
 }
 
 function play {
@@ -19,6 +23,6 @@ function play {
 	
 	##
 	get_question 1
-	get_question 3
+	#get_question 3
 }
 play
